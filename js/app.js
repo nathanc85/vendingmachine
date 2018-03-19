e528da82232a34ab2fd1b7549c94cdeb0f635cbb
Nathan Costea <cnlnat@yahoo.com>
2018-03-18 22:52:48 -0400

    Get and Set for the Quarter class.

--- js/app.js
+++ js/app.js
@@ -5,4 +5,24 @@ var VendingMachine = (function () {
 }());
 /// <reference path="vendingMachine.ts" />
 var machine = new VendingMachine();
+var Quarter = (function () {
+    function Quarter() {
+        this.value = .25;
+    }
+    Object.defineProperty(Quarter.prototype, "Value", {
+        get: function () {
+            return this.value;
+        },
+        set: function (newValue) {
+            this.value = newValue;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Quarter.prototype.getImageUrl = function () {
+        return "img/Quarter.png";
+    };
+    return Quarter;
+}());
+var coin = new Quarter();
 //# sourceMappingURL=app.js.map
--- ts/coin.ts
+++ ts/coin.ts
@@ -1,5 +1,13 @@
 class Quarter {
-    private value: number = .25;
+    private value= .25;
+
+    get Value() {
+        return this.value;
+    }
+
+    set Value(newValue) {
+        this.value = newValue;
+    }
     getImageUrl(): string {
         return "img/Quarter.png";
     }
