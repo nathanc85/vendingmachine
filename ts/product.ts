423cfe0860aa02c96baa526ddf58f25a55057b0c
Nathan Costea <cnlnat@yahoo.com>
2018-03-21 00:06:20 -0400

    Added the Vending's Cells. I'm still debugging an issue with the site.css file.

---
+++ ts/product.ts
@@ -0,0 +1,8 @@
+/// <reference path="productCategory.ts" />
+
+
+class CocaCola {
+    name = 'Coca-Cola';
+    price = 2.2;
+    category = new SodaCategory();
+}
--- ts/vendingMachine.ts
+++ ts/vendingMachine.ts
@@ -1,8 +1,33 @@
 /// <reference path="./coin.ts" />
+/// <reference path="./product.ts" />
+/// <reference path="typings/knockout.d.ts" />
+/// <reference path="productFactory.ts" />
 
+
+
+enum VendingMachineSize {
+    small = 6,
+    medium = 9,
+    large = 12,
+}
+
+class Cell {
+    constructor(public product: CocaCola) {}
+
+    stock = ko.observable(3);
+    sold = ko.observable(false);
+}
 
 class VendingMachine {
     private paid = ko.observable(0);
+    cells = ko.observableArray([]);
+    set size(givenSize: VendingMachineSize) {
+        this.cells([]);
+        for (let index = 0; index < givenSize; index++) {
+            let product = productFactory.GetProduct();
+            this.cells.push(new Cell(product));
+        }
+    }
     acceptedCoins: Quarter[] = [new Quarter()];
     acceptCoin = (coin: Quarter) => {
         // Works the same but the code is shorter.
